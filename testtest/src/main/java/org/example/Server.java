fefd79413938ea21c4e93285e76060b5d8c36bc5
Volkov <vad.volkov91@gmail.com>
2024-09-18 03:43:16 +0700

    update_2

---
+++ testtest/src/main/java/org/example/Server.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final int theardCount = 64;
+    final ExecutorService threadPool = Executors.newFixedThreadPool(theardCount);
+    private final int port;
+    private final Map<String, Map<String, Handler>> handlersMap = new ConcurrentHashMap<>();
+
+
+
+    public Server(int port) {
+
+        this.port = port;
+    }
+
+    public void start() {
+        final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
+                "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html",
+                "/classic.html", "/events.html", "/events.js");
+
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен");
+            while (true) {
+                try {
+                    final Socket socket = serverSocket.accept();
+                    Runnable serverRunnable = new ServerRunnable(socket, validPaths, this);
+                    threadPool.submit(serverRunnable);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        Map<String, Handler> pathMap = handlersMap.get(method);
+        if (null == pathMap) {
+            pathMap = new ConcurrentHashMap<>();
+            pathMap.put(path, handler);
+            handlersMap.put(method, pathMap);
+            return;
+        }
+        pathMap.putIfAbsent(path, handler);
+    }
+
+    public Handler getHandler(String method, String path) {
+        Map<String, Handler> pathMap = handlersMap.get(method);
+        if (null == pathMap) return null;
+        return pathMap.get(path);
+    }
+
+    public void printHandlers() {
+       // System.out.println("Список добавленных хендлеров: ");
+        for (Map.Entry<String, Map<String, Handler>> entry : handlersMap.entrySet()) {
+            System.out.print(entry.getKey() + ": ");
+            for (Map.Entry<String, Handler> innerEntry : entry.getValue().entrySet()) {
+                System.out.print(innerEntry.getKey() + "; ");
+            }
+            System.out.println();
+        }
+    }
+}
